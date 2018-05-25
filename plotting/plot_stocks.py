b5931f5608718e9b1cc0b1f7448768ff4716d0cb
Milan Curcic <caomaco@gmail.com>
2018-05-25 12:56:56 -0400

    add basic plotting script

---
+++ plotting/plot_stocks.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.use('Agg')
+
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+class Stock():
+    def __init__(self, filename):
+        self.time = []
+        self.open = []
+        self.high = []
+        self.low = []
+        self.close = []
+        self.adjclose = []
+        data = [line.strip() for line in open(filename, 'r').readlines()][1:]
+        for line in data[::-1]:
+            line = line.split(',')
+            self.time.append(datetime.strptime(line[0], '%Y-%m-%d'))
+            self.open.append(float(line[1]))
+            self.high.append(float(line[2]))
+            self.low.append(float(line[3]))
+            self.close.append(float(line[4]))
+            self.adjclose.append(float(line[5]))
+
+
+datapath = '../data'
+
+stocks = ['AAPL', 'AMZN', 'CRAY', 'CSCO', 'HPQ', 'IBM', 'INTC', 'MSFT', 'NVDA', 'ORCL']
+
+for stock in stocks:
+    s = Stock(datapath + '/'+stock+'.csv')
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(111, ylim=(0, max(s.close)))
+    ax.tick_params(axis='both', labelsize=16)
+    plt.plot(s.time, s.close, 'k-', lw=1)
+    plt.plot(s.time, s.adjclose, 'r-', lw=1)
+    plt.title(stock, fontsize=16)
+    plt.ylabel('Price', fontsize=16)
+    plt.grid(True)
+    plt.savefig(stock + '.png', dpi=100)
+    plt.close(fig)
