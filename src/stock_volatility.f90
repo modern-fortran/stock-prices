0a447fd9927dd152863b2bbe84256d2ba5e0702c
Milan Curcic <caomaco@gmail.com>
2018-06-17 11:10:05 -0400

    wrap up

--- src/stock_volatility.f90
+++ src/stock_volatility.f90
@@ -1,6 +1,6 @@
 program stock_volatility
 
-  use mod_arrays, only: moving_average, moving_std
+  use mod_arrays, only: average, std, moving_average, moving_std, reverse
   use mod_io, only: read_stock, write_stock
 
   implicit none
@@ -9,7 +9,6 @@ program stock_volatility
   character(len=10), allocatable :: time(:)
   real, allocatable :: open(:), high(:), low(:), close(:), adjclose(:), volume(:)
   integer :: i, im, n
-  real :: volatility
 
   symbols = ['AAPL', 'AMZN', 'CRAY', 'CSCO', 'HPQ ',&
              'IBM ', 'INTC', 'MSFT', 'NVDA', 'ORCL']
@@ -19,20 +18,20 @@ program stock_volatility
     call read_stock('data/' // trim(symbols(n)) //  '.csv', time,&
       open, high, low, close, adjclose, volume)
 
-    !adjclose = reverse(adjclose)
-    !volatility = std(adjclose)
+    im = size(time)
+    adjclose = reverse(adjclose)
 
     if (n == 1) then
-      print *, time(size(time)) // ' through ' // time(1)
+      print *, time(im) // ' through ' // time(1)
       print *, 'Symbol, Average (USD), Volatility (USD), Relative Volatility (%)'
       print *, '----------------------------------------------------------------'
     end if
 
-    !print *, symbols(n), average(adjclose), std(adjclose),&
-    !  nint(std(adjclose) / average(adjclose) * 100)
+    print *, symbols(n), average(adjclose), std(adjclose),&
+      nint(std(adjclose) / average(adjclose) * 100)
 
-    call write_stock(trim(symbols(n)) // '.csv', time, adjclose,&
-                     moving_average(adjclose, 100), moving_std(adjclose, 100))
+    call write_stock(trim(symbols(n)) // '.csv', time(im:1:-1), adjclose,&
+      moving_average(adjclose, 30), moving_std(adjclose, 30))
 
   end do
 
